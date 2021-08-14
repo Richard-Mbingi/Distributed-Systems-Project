d7473c3c73b14e1449bd6d0aa70fb6b37966c034
Richard-Mbingi <mbingicoding@gmail.com>
2021-08-14 13:27:49 +0300

    Server & client two way communication. Both server and client can send messages

--- src/Client.java
+++ src/Client.java
@@ -3,16 +3,32 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        //Create client socket
         Socket s = new Socket("localhost", 4999);
 
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("is it working");
-        pr.flush();
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 
+        //Reads data coming form the server
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
 
-        String str = bf.readLine();
-        System.out.println("client : " + str);
+        //Reads data from the keyboard
+        BufferedReader kb = new BufferedReader(
+                new InputStreamReader(System.in)
+        );
+
+        String str, str1;
+
+        //repeat as long as logout is not typed at client
+        while (!(str = kb.readLine()).equals("logout")) {
+            //send to server
+            dos.writeBytes(str + "\n");
+
+            //receive from the server
+            str1 = bf.readLine();
+
+            System.out.println(str1);
+        }
+
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -6,16 +6,42 @@ public class Server{
         ServerSocket ss = new ServerSocket(4999);
         Socket s = ss.accept();
 
-        System.out.println(("client connected"));
+        System.out.println(("Client connected"));
 
+
+        //Sends data to client
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        //Reads data coming from client
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
 
-        String str = bf.readLine();
-        System.out.println("server : " + str);
+        //Reads data from the keyboard
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
 
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("yes");
-        pr.flush();
+        //server executes continuously
+        while (true) {
+            // repeats as long as client does not send a null string
+            String str, str1;
+
+            //read from client
+            while ((str = bf.readLine()) != null) {
+                System.out.println(str);
+                str1 = kb.readLine();
+
+                //send to client
+                ps.println(str1);
+            }
+
+            //Closes connection
+            ps.close();
+            bf.close();
+            kb.close();
+            ss.close();
+            s.close();
+
+            //terminate application
+            System.exit(0);
+        }
     }
 }
