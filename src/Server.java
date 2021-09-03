c7bbb94735fb534243de9de693fc9c32b5ae365b
Richard-Mbingi <mbingicoding@gmail.com>
2021-09-03 21:27:52 +0300

    Random number generator

--- src/Server.java
+++ src/Server.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Server {
     public static void main(String[] args) throws IOException {
@@ -12,9 +15,16 @@ public class Server {
         jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
 
+        Random random = new SecureRandom();
+        int rand = random.nextInt(30000);
+
+        System.out.println(rand);
+
         JLabel jLabelText = new JLabel("Waiting for input from client");
+        JLabel jPersonalCodeText = new JLabel("Your personal code: " + rand);
 
         jFrame.add(jLabelText, BorderLayout.SOUTH);
+        jFrame.add(jPersonalCodeText, BorderLayout.CENTER);
 
         jFrame.setVisible(true);
 
